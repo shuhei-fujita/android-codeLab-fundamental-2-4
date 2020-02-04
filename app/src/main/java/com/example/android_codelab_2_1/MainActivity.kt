aa7e064b2506ff89ec389505dd0cae368d7c2de1
syuheifujita <mcslr772ed@gmail.com>
2020-02-05 03:21:51 +0900

    キーボードを隠す

--- app/src/main/java/com/example/android_codelab_2_1/MainActivity.kt
+++ app/src/main/java/com/example/android_codelab_2_1/MainActivity.kt
@@ -1,12 +1,55 @@
 package com.example.android_codelab_2_1
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val buttonDone = findViewById<Button>(R.id.done_button)
+        val nicknameTextView = findViewById<TextView>(R.id.nickname_text)
+
+        buttonDone.setOnClickListener {
+            addNickname(it)
+        }
+
+        nicknameTextView.setOnClickListener() {
+            updateNickname(it)
+        }
+    }
+
+    private fun addNickname(view: View) {
+        val editText = findViewById<EditText>(R.id.nickname_edit)
+        val nicknameTextView = findViewById<TextView>(R.id.nickname_text)
+        val buttonDone = findViewById<Button>(R.id.done_button)
+
+        nicknameTextView.text = editText.text
+
+        editText.visibility = View.GONE
+        view.visibility = View.GONE
+        nicknameTextView.visibility = View.VISIBLE
+
+//        キーボードを隠す
+        val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+    }
+
+    private fun updateNickname (view: View) {
+        val editText = findViewById<EditText>(R.id.nickname_edit)
+        val doneButton = findViewById<Button>(R.id.done_button)
+
+        editText.visibility = View.VISIBLE
+        doneButton.visibility = View.VISIBLE
+        view.visibility = View.GONE
     }
 }
